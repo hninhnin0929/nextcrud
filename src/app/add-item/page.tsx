5a085cd232af0683aaed9587b29a35c6d211a28c
hnin <hnin0929@ecplaza.net>
2024-03-19 14:32:26 +0900

    styling add item page

--- src/app/add-item/page.tsx
+++ src/app/add-item/page.tsx
@@ -1,6 +1,20 @@
 const AddItem = () => {
     return (
-        <div>AddItem</div>
+        <div>
+            <form className="flex flex-col mt-2 gap-3">
+                <input type="text"
+                    id="title"
+                    className="border border-orange-500 px-7 py-2"
+                    placeholder="Enter item title" />
+                <input type="text"
+                    id="description"
+                    className="border border-orange-500 px-7 py-2"
+                    placeholder="Enter item description" />
+                <button type="submit" className="bg-slate-700 font-bold text-white px-7 py-2 w-fit">
+                    Submit
+                </button>
+            </form>
+        </div>
     )
 }
 
