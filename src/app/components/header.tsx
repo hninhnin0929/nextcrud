34edb3fb9d7f3b5d46dab8fd3addeed4ce2055bb
hnin <hnin0929@ecplaza.net>
2024-03-19 14:24:48 +0900

    add item page

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div>
         <nav className='flex justify-between items-center bg-orange-600 px-8 py-4'>
             <Link className='text-white font-bold text-2xl' href={"/"}>NextCrud</Link>
-            <Link className='bg-slate-700 p-2 text-white font-bold' href={"/addItem"}>Add Item</Link>
+            <Link className='bg-slate-700 p-2 text-white font-bold' href={"/add-item"}>Add Item</Link>
         </nav>
     </div>
   )
